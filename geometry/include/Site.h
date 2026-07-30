e67625a3a567e0a8b0c67d56a79494ba58c2a209
ssboy1818 <maximov.asd8@gmail.com>
2026-07-31 00:15:54 +0500

    feat: add Site class.

---
+++ geometry/include/Site.h
@@ -0,0 +1,96 @@
+#pragma once
+
+#include "Vector2.h"
+
+namespace {
+
+inline static std::size_t newId() noexcept {
+    static std::size_t id_ = 1;
+    return id_++;
+}
+
+} // namespace
+
+class Site {
+public:
+    std::size_t id{newId()};
+    Vector2d position;
+
+public:
+    Site() noexcept = default;
+    Site(double x, double y) noexcept;
+    Site(Vector2d position_) noexcept;
+
+    Site(const Site &other) noexcept;
+
+    Site(Site &&other) noexcept;
+
+    ~Site() noexcept = default;
+
+    Site &operator=(const Site &other) noexcept;
+
+    Site &operator=(Site &&other) noexcept;
+
+    bool operator<(const Site &other) const noexcept;
+    bool operator>(const Site &other) const noexcept;
+    bool operator==(const Site &other) const noexcept;
+};
+
+// Implementation
+
+inline Site::Site(double x, double y) noexcept
+    : position(x, y) {}
+
+inline Site::Site(Vector2d position_) noexcept
+    : position(position_) {}
+
+inline Site::Site(const Site &other) noexcept
+    : position(other.position) {}
+
+inline Site::Site(Site &&other) noexcept
+    : id(other.id), position(other.position) {
+    other.id = 0;
+    other.position = {0, 0};
+}
+
+inline Site &Site::operator=(const Site &other) noexcept {
+    if (this == &other)
+        return *this;
+
+    position = other.position;
+    return *this;
+}
+
+inline Site &Site::operator=(Site &&other) noexcept {
+    if (this == &other)
+        return *this;
+
+    id = other.id;
+    position = other.position;
+
+    other.id = 0;
+    other.position = {0, 0};
+
+    return *this;
+}
+
+inline bool Site::operator<(const Site &other) const noexcept {
+    if ((position.x < other.position.x) ||
+        (position.x == other.position.x && position.y < other.position.y))
+        return true;
+    return false;
+}
+
+inline bool Site::operator>(const Site &other) const noexcept {
+    if ((position.x > other.position.x) ||
+        (position.x == other.position.x && position.y > other.position.y))
+        return true;
+    return false;
+}
+
+inline bool Site::operator==(const Site &other) const noexcept {
+    return position.x == other.position.x
+        && position.y == other.position.y;
+}
+
+
